7a216557b82041c26141ba2ed88698a2b597056c
nyi nyi zin <nyinyizin1818@gmail.com>
2025-06-27 13:21:59 +0630

    using auth dtos Done

--- nest-api/src/auth/auth.controller.ts
+++ nest-api/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
-    console.log(dto);
+  signup(@Body() dto: AuthDto) {
+    dto.email;
     return this.authService.signup();
   }
 
